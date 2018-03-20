2abc186695340451fce896bec9a64a990c41593e
Juraj Chlebec <juraj.chlebec@gmail.com>
2018-03-20 18:56:39 +0100

    dynamically generate dataframes from excel sheets

--- first.R
+++ first.R
@@ -2,3 +2,11 @@ print('aaa')
 print('bbb')
 print('ccc')
 print('ddd')
+
+
+zoznam <- c('BL', 'TT', 'TN')
+
+for (el in zoznam) {
+  assign(el, readxl::read_xlsx('c:/downloads/1xml_statistika_podla_druhu_kriminality_1_31.01.2017.xlsx', skip = 6, sheet = el))
+}
+
